6e180be70fa61556d3eb809a8b1f69fb8929fa5f
rafiibro <rafael.dacunhagarcia@icloud.com>
2019-05-28 14:27:55 +0200

    XML Parser

---
+++ src/main/java/XMLParser.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdom2.*;
+import org.jdom2.input.*;
+
+public class XMLParser {
+
+    XMLParser(String path) {
+
+        SAXBuilder saxBuilder = new SAXBuilder();
+        File xmlFile = new File(path);
+        List<Pays> listPays = new ArrayList<Pays>();
+
+        try {
+            Document document = saxBuilder.build(xmlFile);
+            Element rootElement = document.getRootElement();
+
+            List listCountries = rootElement.getChildren("element");
+
+            for (int iCountry = 0; iCountry < listCountries.size(); ++iCountry) {
+
+                Element country = (Element) listCountries.get(iCountry);
+                Pays p = new Pays();
+                p.setPopulation(Integer.parseInt(country.getChild("population").getValue()));
+                p.setNom(country.getChild("translations").getChild("fr").getValue());
+                p.setCapitale(country.getChild("capital").getValue());
+                p.setContinent(country.getChild("region").getValue());
+                p.setSousContinent(country.getChild("subregion").getValue());
+                p.setDrapeau(country.getChild("flag").getValue());
+                if (!country.getChild("area").hasAttributes()) {
+                    p.setSuperficie(Double.parseDouble(country.getChild("area").getValue()));
+                }
+                List langues = country.getChild("languages").getChildren("element");
+                for (int iLangue = 0; iLangue < langues.size(); ++iLangue) {
+                    Element langue = (Element) langues.get(iLangue);
+                    p.addLangues(langue.getChild("name").getValue());
+                }
+                listPays.add(p);
+            }
+        }
+
+        catch (IOException io) {
+            System.out.println(io.getMessage());
+        }
+
+        catch (JDOMException jdomex) {
+            System.out.println(jdomex.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        XMLParser x = new XMLParser("/Users/rafael.dacunhag/Desktop/SER/LABO_4_XSLT_XPATH/src/main/java/countries.xml");
+    }
+}
